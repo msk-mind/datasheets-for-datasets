af56fc372622afe8e24b647cb7ef008c77208989
kohlia <kohlia@mskcc.org>
2024-12-23 15:52:29 -0500

    adding copath molecular table datasheet

--- pathology-data-mining/copath_molecular_links_cleaned.md
+++ pathology-data-mining/copath_molecular_links_cleaned.md
@@ -13,7 +13,8 @@
 
 <b>Summary Statistics:</b>
 
-TODO: Update:  Shape and size of table, counts of key data elements and other statistics that may be useful for the user. 
+Total number of rows (specimen numbers): 186,786
+
 
 
 # Table of contents
@@ -23,37 +24,68 @@ TODO: Update:  Shape and size of table, counts of key data elements and other st
 
 ## Description <a name="description"></a>
 
-TODO UPDATE: Explain the purpose for which the dataset was created.  This should include the motivation
-for the dataset, its composition (any relationships between fields of the dataset),
-the collection process (if applicable), expected uses of the data, and so on.  If the dataset was composed
-from other datasets, its lineage should be clearly described, with references to the
-parent datasets.
+Table containing parsed and cleaned columns from [the source copath molecular links table](https://github.com/msk-mind/datasheets-for-datasets/blob/main/hobbit/copath_molecular_links.md). The `part_description` field is split up and parsed into separate columns that indicate the part and block numbers, as well as other potentially useful metadata columns. All columns in this table other than `specnum_formatted` are parsed from the `part_description` field. The process is described in the sql query.
+
+In short, the structured portion of the `part_description` contains information pertaining to the part and block of the tissue sample, as well as information about staining and the date of procedure. The structured portion is split up into metadata fields depending on what is available. 
+
+For example, take the following `part_description`:
+```
+ Liver, lateral  section (MSK:S29-2342/23-1-a, 20 USS and 1H&E, DOP:02/03/2029)
+ ```
+ In this case, the portion in parentheses is extracted and the following fields are extracted:
+ ```
+ s_number: S29-2342
+ part_number: 23
+ s_blkdesig_label: 1-a
+ dop: 02/03/2029
+ hne_count: 1
+ uss_count: 20
+ ```
+ Not all `part_description` values are this easily parsed and there is variation that leads to some values being unavailable. 
+
+ Of all available samples (186,786), 42,295  come from external sources, and of them, 2880 only have an external accession.
+
 
 ### Vocabulary <a name="vocab"></a>
 
-TODO: Update: The vocabulary of a dataset is comprised of its variables, (or field names) the description (or semantics) of 
-these variables, their field type, data type and format. An unambiguous field description
-is very important for consistent interpretation of the field. The description must also include units where applicable. 
-Equally important are the field types. The field type provides the semantics behind the variable. The data type specifies the encoding that should be used for the variable when the dataset is loaded into memory. The encoding is critical because the data may be presented in a file format like CSV, which carries no information about the encoding, forcing data loaders (like Pandas) to guess.  Writing a field in the wrong format can lead to terrible and unexpected errors, like the infamous MRN zero-padding error where the preceeding zeros in an MRN are stripped if the field is encoded as an integer instead of a string. The field format may be used to further describe or elaborate on categorical variables that have a finite set of possible values and semantics behind each value. 
 
 | **Field name** | **Description** | **Field Type** | **Data Type** | **Field Format** |
 |---|---|---|---|---|
 | specnum_formatted | 'M' accession number from DMP or Molecular Number | ID | string | |
-| specclass_id |  | string  | Categorical | [DMPN, DM, MPATH]|
-| part_inst | part id from surgical accession | ID  | integer  | |
-| part_type | tissue type | string | categorical  | See below |
-| part_description | free text followed by machine readable form in parenthesis with link to Surgical accession ID, part number and block designator that can be used to link an IMPACT sample to a specific block | ID  | string  | |
-| description | parsed out part description | ID  | integer  | |
-| s_number | parsed out surgical accession ID | ID  | integer  | |
+| part_description | free text followed by machine readable form in parenthesis with link to Surgical accession ID, part number and block designator that can be used to link an IMPACT sample to a specific block | string  | string  | |
+| s_number | parsed out internal surgical accession ID  | ID  | integer  | |
 | part_number | parsed out surgical part number | ID  | integer  | |
 | s_blkdesig_label | parsed out block label from surgical accession | ID | string  | |
-| outside_number | parsed out non-MSK accession number | ID  | string  | |
 | dop | parsed out date of procedure | Date | date | |
-| uss | parsed out unstained slide count | integer | integer | |
-| hne | parsed out h&E slide count | integer | integer | |
+| uss_count | parsed out unstained slide count | integer | integer | |
+| hne_count | parsed out h&E slide count | integer | integer | |
+| has_only_external_s_number | Indicates if a specnum only has an external accession number | Binary  | bool  | |
+| has_external_s_number | Indicates if a specnum has an external accession number | Binary  | bpp;  | |
 
 
 
 ## Notes <a name="notes"></a>
 
+1. Not all `part_description` values contain a DOP, and in some cases there is no `DOP` label despite a date being indicated. These cases are not captured in the `DOP` column
+2. If present, the `uss_count` and `hne_count` fields are typically structured where `uss_count` is in the 15-20 range, and the `hne_count` is usually 1. Sometimes, the field is clearly incorrect and `hne_count` is 20 and `uss_count` is 1 (for example), which is a user entry mistake. 
+3. Often times, when neither `uss_count` or `hne_count` are present `part_description` field just says something like `1 paraffin block` and no other details on staining. 
+4. Many cases that come from external sources often get internal accessions. In those cases, both the internal and external accession is available, but only the internal accession is pulled out. In order to flag these cases, use the `has_external_s_number`
+5. Some external cases are also not given internal accessions. To flag these cases, use the `only_has_external_s_number`
+6. Some internal and external accessions are formatted similarly (ie both are M\d+-[a set of digits])
+7. Approximately 38,000 specnums do not have part_descriptions that contain structured data (for example: blood samples, normal samples, cfDNA, Nail, donor samples, )
+
+7. The following columns contain missing values:
+```
+specnum_formatted: 0
+part_description: 1
+description:
+s_number: 38,089 
+part_number: 51,622
+s_blk_desig_label: 98,946
+dop: 122,429
+uss_count: 55,879
+hne_count: 58,043
+only_has_external_s_number: 0
+has_external_s_number: 0
+```
 
--- pathology-data-mining/sql/copath_molecular_links_cleaned.sql
+++ pathology-data-mining/sql/copath_molecular_links_cleaned.sql
@@ -1,28 +1,107 @@
 WITH
-  t1 AS ( -- filter out rows not containing bracketed portion with structured data. these rows are typically bone marrow and normal blood samples  
-SELECT 
-specnum_formatted, 
-part_inst, 
-part_type, 
-part_description, 
-REGEXP_LIKE(TRIM(part_description), '.*\(\s*C\d+|S\d+|MSKS:|MKS:|MS|ms|Outside:.+\)$') as bool 
-FROM "hobbit-poc"."molecular_cases" WHERE bool
+  t1 as 
+  ( -- Filter out rows not containing structured data. This is split into two steps. 
+    -- First, check to see if there are matches on keywords inside parenthases 
+    -- that indicate precense of a valid accession
+        select 
+        specnum_formatted,
+        part_type, 
+        part_description, 
+        REGEXP_extract(part_description, '.*\(\s*InternalVal|InternalVal|ExternalVal|Outisde|Oustide|Outside|MSK: MSK: |MSKp:|MSK;|MSK:|MSKS|MKS|MSK:MSK|MSK|MSk|msk|MS|MS|MS.+\)$', 0) as matched
+        from "hobbit-poc"."molecular_cases"
   ),
-  t2 AS (
-      SELECT -- split free text from bracketed portion into separate array elements
-specnum_formatted, 
-part_inst, 
-part_description,
-regexp_split(part_description, '\(\s*C\d+|S\d+|MSKS:|MKS:|MS|ms|Outside', 'FIRST', -1) AS structured_data
-FROM t1
+  t2a as 
+  ( -- If there is not a match on one of the the primary keywords, then check to see if there is
+    -- still an accession present that doesn't have a valid prefix
+        select specnum_formatted, part_type, part_description, matched, 
+        REGEXP_extract(part_description,  '.*\(\s*MS|C\d+|S\d+|M\d+|CD\d+|NY\d+|A\d+|80\-|MS.+\)$', 0) as _t2a_matched,
+        REGEXP_split(part_description, '.*\(\s*MS|C\d+|S\d+|M\d+|CD\d+|NY\d+|A\d+|80\-|MS.+\)$', 'FIRST', -1) as _t2a_parsed,
+        _t2a_matched || ARRAY_TO_STRING(SUBLIST(_t2a_parsed, 2, 1),',') as parsed
+        from t1 where matched = ''
   ),
-  t3 AS ( 
-      SELECT 
-      specnum_formatted, 
-      part_inst, 
-      part_description,
-      ARRAY_TO_STRING(SUBLIST(structured_data, 1, 1),',')as free_text_portion,
-      ARRAY_TO_STRING(SUBLIST(structured_data, 2, 1),',') as structured_portion  
-      FROM t2
+  t2b as
+  ( -- Match on the primary keywords
+        select specnum_formatted, part_type, part_description, matched, 
+        REGEXP_split(part_description, '.*\(\s*InternalVal|InternalVal|ExternalVal|Outisde|Oustide|Outside|MSK: MSK: |MSKp:|MSK;|MSK:|MSKS|MKS|MSK:MSK|MSK|MSk|msk|MS|MS.+\)$', 'FIRST', -1) as _t2a_parsed,
+        ARRAY_TO_STRING(SUBLIST(_t2a_parsed, 2, 1),',') as parsed from t1 where matched <> '' 
+  ),
+  t3 as
+  ( -- Union the two previous steps so that all of the specnums that have valid into parsed column
+        select specnum_formatted, part_type, part_description, matched, parsed from t2b
+        union all
+        select specnum_formatted, part_type, part_description, matched, parsed from t2a
+  ),
+  t4 as 
+  ( -- Trim trailing characters
+        select specnum_formatted, part_description, matched, 
+        COALESCE(NULLIF(trim(
+        trim(trailing ')' from 
+            trim(leading ':' from 
+            parsed))), ''),'') as structured_portion
+            
+        from t3
+  ),
+  t5 as 
+  ( -- Extracts number adjacent to either H&E or USS labels. 
+    -- In rare cases, these numbers are swapped (ie 20 H&E, 1 USS), but more often there is no count for either value. 
+        select specnum_formatted, part_description, structured_portion, matched,
+        REGEXP_Extract(structured_portion, '(\d+)\s*(H&E|H/E)') as hne_count,
+        REGEXP_Extract(structured_portion, '(\d+)\s*(USS|US|unstained|uss|uSS|and)') as uss_count from t4
+  ),
+  t6 as 
+  (-- Extract date of procedure based on looking at dates adjacent to DOP character. 
+   -- Not all specnums have a DOP, and not all DOPs are prefixed correctly. 
+        select specnum_formatted, part_description, structured_portion, uss_count, hne_count, matched, 
+        REGEXP_EXTRACT(structured_portion, 'DOP:?\s*(\d{1,2}/\d{1,2}/\d{2,4})') as dop
+        from t5
+  ),
+  t7 as 
+  ( -- Extract binary variable on 'matched', which indicates if a specnum has only an external sample  
+    --not where it has both an external and internal sample)            
+        select specnum_formatted, part_description, structured_portion, uss_count, hne_count, matched, dop,
+        case matched
+            when 'ExternalVal' then True
+            when 'Outside' then True
+            when 'Oustide' then TRUE
+            when 'Outisde' then True
+            else False
+        end as has_only_external_s_number
+        from t6
+  ),
+  t8 as 
+  (-- Extracts surgical part designator from the accession by taking all characters to the right of the first comma or space in the structured_portion.
+        select specnum_formatted, part_description, structured_portion, uss_count, hne_count, matched, has_only_external_s_number, dop,
+        TRIM(
+            TRIM(leading '"' from 
+            TRIM(leading ':' from 
+            TRIM(leading 'l:' from 
+            regexp_extract(structured_portion, '^(.*?)(?:,|\s|$)'))))) as surg_part_desig from t7
+  ),
+  t9 as 
+  ( -- Parses out the part and block portions of the surg_part_desig, which is everything to the left of the first / 
+    -- In some cases, internal and external accessions cannot be separated. 
+        select specnum_formatted, part_description, structured_portion, uss_count, hne_count, matched, has_only_external_s_number, surg_part_desig, dop,
+        regexp_extract(surg_part_desig, '^(.*?)(?=/|$)') as s_number,
+        regexp_extract(surg_part_desig, '\/(.*)$') as part_block_desig
+        from t8
+  ),
+  t10 as 
+  ( -- Separate part and block from part_block deisgnator on the - character 
+        select specnum_formatted, part_description, structured_portion, uss_count, hne_count, matched, has_only_external_s_number, dop,
+        surg_part_desig, s_number, part_block_desig, regexp_extract(part_block_desig, '^(.*?)(?:,|\s|$)') as part_number,
+        regexp_extract(part_block_desig, '-(.*)$') as s_blk_desig_label from t9
+  ),
+  t11 as 
+  ( -- Binary variable checked against the entire structured_portion field that indicates if the sample has an external surgical part designator
+        select specnum_formatted, part_description, structured_portion, uss_count, hne_count, matched, has_only_external_s_number, 
+        surg_part_desig, s_number, part_block_desig, part_number, s_blk_desig_label, dop,
+        case 
+            when structured_portion LIKE '%ExternalVal%' then True
+            when structured_portion LIKE '%Outside%' then True
+            when structured_portion LIKE '%Oustide%' then True
+            when structured_portion LIKE '%Outisde%' then True
+            else False
+        end as has_external_s_number
+        from t10
   )
-  SELECT * FROM t3 
+select specnum_formatted, part_description, s_number, part_number, s_blk_desig_label, uss_count, hne_count, has_only_external_s_number from t11
